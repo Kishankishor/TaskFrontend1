2f9f54de8472341a20d863ec1f8c46f5d1d26991
Kishankishor <92016928+Kishankishor@users.noreply.github.com>
2023-12-15 11:39:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { FormInput } from "./forminput/FormInput";
+
+
+function App() {
+  return (
+    <div className="App">
+    <FormInput/>
+   
+    </div>
+  );
+}
+
+export default App;
+
+
